632964d4454f094a6a9696ac80ed560281b804ee
derenay <erenayarsal@hotmail.com>
2025-01-31 13:33:52 +0300

    selamlar

--- regresyon.py
+++ regresyon.py
@@ -10,12 +10,46 @@ from sklearn.metrics import r2_score
 
 df = pd.read_csv('clear_data.csv')
 
+categorical_features = ["city", "district", "neighbourhood"]
+numerical_features = ["room", "living_room", "size", "age", "floor"]
 
 
 
+full_pipline = ColumnTransformer([
+    ('num', StandardScaler(), numerical_features),
+    ('cat', OneHotEncoder(handle_unknown='ignore'), categorical_features)
+])
 
 
 
+X = df.drop('price', axis=1)
+y = df['price']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = Pipeline([
+    ('preprocessing', full_pipline),
+    ('model', LinearRegression())
+])
+
+
+model.fit(X_train, y_train)
+
+
+y_pred = model.predict(X_test)
+mse = mean_squared_error(y_test, y_pred)
+rmse = np.sqrt(mse)
+r2 = r2_score(y_test, y_pred)
+
+
+print(f"MSE: {mse}")
+print(f"RMSE: {rmse}")
+print(f"R2: {r2}")
+
+
+
+feature_importance = model.named_steps['model'].coef_
+print(feature_importance)
 
 
 
